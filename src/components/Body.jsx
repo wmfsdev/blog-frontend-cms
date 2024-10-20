fe2ed1fe75ee7d119d5642068af8868ad57a6432
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-10-21 00:08:46 +0100

    change URL params check from /login to /articles route

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,13 +1,14 @@
 import { useParams } from "react-router-dom";
 import Login from "./Login";
+import Articles from "./Articles";
 
 const Body = () => {
     const { name } = useParams();
   
       return (
       <>
-        {name === "login" ? (
-          <Login />
+        {name === "articles" ? (
+          <Articles />
         ) : (
           <DefaultProfile />
         )}
