2233df65bbf478cea16f80c95c3432969e8a4389
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-10-21 00:20:24 +0100

    create route for Articles component with matching loader

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,6 +2,8 @@
 import App from "./App"
 import Body from "./components/Body"
 import Login from "./components/Login"
+import Articles from "./components/Articles"
+import { articlesLoader } from "./util/loader"
 
 
 const routes = [
@@ -9,10 +11,12 @@ const routes = [
         path: "/",
         element: <App />,
         children: [
-            { path: "/", element: <Login /> },
-            { path: "/:name", element: <Body /> },
+        //  { path: "/", element: <Login /> },
+        //  { path: "/:name", element: <Body /> },
+           
         ]
-    }
+    },
+     { path: "/articles", loader: articlesLoader, element: <Articles /> }
 ]
 
 export default routes
