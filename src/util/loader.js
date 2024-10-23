f336a34708e2d2e372b9f0378dbf849d50be23f5
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-10-23 20:58:56 +0100

    update articlesLoader with relevant headers for fetch request

--- src/util/loader.js
+++ src/util/loader.js
@@ -1,8 +1,14 @@
 
 // get all articles
 export async function articlesLoader() {
+    const token = localStorage.getItem("token")
     try {
-        const response = await fetch(`${import.meta.env.VITE_API_URL}/articles`, { mode: 'cors' }) 
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/articles`, { 
+            method: "GET",
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8', 'Authorization': `Bearer ${token}`
+            },
+        })
         const data = await response.json()
         return data
     } catch(err) {
