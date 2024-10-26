0108f0b8c9016043858e0bc3bc06ccc9b052ecb2
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-10-26 23:47:30 +0100

    add buttons for deleting and updating status of articles

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,21 +1,67 @@
-import { Link, useLoaderData } from 'react-router-dom'
+import { Link, useLoaderData, useNavigate } from 'react-router-dom'
 
 const Articles = () => {
-  
-    const articles = useLoaderData()
 
+    const navigate = useNavigate()
+    const articles = useLoaderData()
+    const token = localStorage.getItem("token");
+
+    const handleSubmit = (e, method) => {
+        const data = new FormData(e.target)
+        const articleId = data.get("articleId")
+        handleArticle(method, articleId)
+    }
+
+    async function handleArticle(method, articleId) {
+        console.log("in article handler: ", method, articleId)
+        console.log(typeof method)
+        try {
+            console.log("try comment DELETE")
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/articles/${articleId}`, {
+                method: method,
+                body: JSON.stringify({
+                    article: articleId,
+                }),
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8', 'Authorization': `Bearer ${token}`
+                },
+            })
+
+        if (response.status === 200) {
+            const data = await response.json()
+            console.log(data)
+            navigate(`/article/${articleId}`)
+        }
+
+        if (response.status === 401) {
+            throw err
+        }
+
+        } catch(err) {
+            console.log(err)
+        }
+    }
+          
       return (
         <>
         <div className='articles'>
             <ul>
                 { articles.map((article) => 
                     <li key={article.id}>
-                        <Link key={article.id} to={`article/${article.id}`}>
+                        <Link key={article.id} to={`${article.id}`}>
                         <div className='article-post'>
                             <h2>{article.title}</h2>
                             <p>{article.body}</p>
                         </div>
                         </Link>
+                        <form method="POST" onSubmit={e => {e.preventDefault(); handleSubmit(e, "PUT")}}>
+                            <input type="hidden" name="articleId" value={article.id} />
+                            <button name="button-test">update</button>
+                        </form>
+                        <form method="POST" onSubmit={e => {e.preventDefault(); handleSubmit(e, "DELETE")}}>
+                            <input type="hidden" name="articleId" value={article.id} />
+                            <button name="button-test">delete</button>
+                        </form>
                     </li>
                 )}
           </ul>
