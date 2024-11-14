4335cab8a10b5e938e1247f0c54c2a29840ca592
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-11-14 23:04:00 +0000

    import Navigation component and restructure routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -5,6 +5,7 @@ import Login from "./components/Login"
 import Articles from "./components/Articles"
 import Article from "./components/Article"
 import Compose from "./components/Compose"
+import Navigation from "./components/Navigation"
 import { articlesLoader, articleLoader } from "./util/loader"
 
 
@@ -17,9 +18,18 @@ const routes = [
         //  { path: "/:name", element: <Body /> }, 
         ]
     },
-    { path: "/articles", loader: articlesLoader, element: <Articles /> },
-    { path: "/articles/:id", loader: articleLoader, element : <Article /> },
-    { path: "/articles/compose", element: <Compose /> }
+    { 
+        path: "/articles", 
+        element: <Navigation />,
+        children: [
+            { path: "/articles", loader: articlesLoader, element: <Articles /> },
+            { path: "/articles/:id", loader: articleLoader, element : <Article /> },
+            { path: "/articles/compose", element: <Compose /> }
+        ]
+    },
+    // { path: "/articles", loader: articlesLoader, element: <Articles /> },
+    // { path: "/articles/:id", loader: articleLoader, element : <Article /> },
+    // { path: "/articles/compose", element: <Compose /> }
 ]
 
 export default routes
