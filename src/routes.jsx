6561e290eddffc9d11a5aae4a01bffd3fe1fb793
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-11-15 23:18:56 +0000

    tidy routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,5 @@
 
 import App from "./App"
-import Body from "./components/Body"
-import Login from "./components/Login"
 import Articles from "./components/Articles"
 import Article from "./components/Article"
 import Compose from "./components/Compose"
@@ -13,10 +11,6 @@ const routes = [
     {
         path: "/",
         element: <App />,
-        children: [
-        //  { path: "/", element: <Login /> },
-        //  { path: "/:name", element: <Body /> }, 
-        ]
     },
     { 
         path: "/articles", 
@@ -27,9 +21,6 @@ const routes = [
             { path: "/articles/compose", element: <Compose /> }
         ]
     },
-    // { path: "/articles", loader: articlesLoader, element: <Articles /> },
-    // { path: "/articles/:id", loader: articleLoader, element : <Article /> },
-    // { path: "/articles/compose", element: <Compose /> }
 ]
 
 export default routes
