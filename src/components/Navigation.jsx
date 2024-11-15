7c7094e1b333dc669ebd239716a9f050bd6d51de
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-11-15 23:20:20 +0000

    add logOut function to Navigation component for clearing token from local storage

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,11 +2,15 @@ import { Link, Outlet } from "react-router-dom"
 
 const Navigation = () => {
 
+  function logOut() {
+    localStorage.clear()
+  }
+
     return (
       <>
       <div className="navigation">
         <Link to="/articles">DASHBOARD</Link>
-        <Link to="/logout">LOGOUT</Link>
+        <Link to="/" onClick={logOut}>LOGOUT</Link>
       </div>
       <Outlet />
       </>
