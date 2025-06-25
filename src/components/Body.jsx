699c837042224e23de3c8f24dadeb7390577a8a1
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2025-06-25 21:03:33 +0100

    fix indentation

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,19 +1,18 @@
 import { useParams } from "react-router-dom";
-import Login from "./Login";
 import Articles from "./Articles";
 
 const Body = () => {
-    const { name } = useParams();
-  
-      return (
-      <>
-        {name === "articles" ? (
-          <Articles />
-        ) : (
-          <DefaultProfile />
-        )}
-      </>
-    );
+  const { name } = useParams();
+
+  return (
+    <>
+      {name === "articles" ? (
+        <Articles />
+      ) : (
+        <DefaultProfile />
+      )}
+    </>
+  );
 };
 
 export default Body
