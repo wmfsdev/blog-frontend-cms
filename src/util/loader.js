1552205829aa19df2a89b384330f82d047dbd04a
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2025-06-25 20:55:55 +0100

    fix indentation

--- src/util/loader.js
+++ src/util/loader.js
@@ -1,31 +1,29 @@
 
-// get all articles
 export async function articlesLoader() {
-    const token = localStorage.getItem("token")
-    try {
-        const response = await fetch(`${import.meta.env.VITE_API_URL}/articles`, { 
-            method: "GET",
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8', 'Authorization': `Bearer ${token}`
-            },
-        })
-        const data = await response.json()
-        return data
-    } catch(err) {
-        return err
-    }
+	const token = localStorage.getItem("token")
+	try {
+		const response = await fetch(`${import.meta.env.VITE_API_URL}/articles`, { 
+			method: "GET",
+			headers: {
+				'Content-type': 'application/json; charset=UTF-8', 'Authorization': `Bearer ${token}`
+			},
+		})
+		const data = await response.json()
+		return data
+	} catch(err) {
+		return err
+	}
 }
 
 export async function articleLoader({ params }) {
+	const token = localStorage.getItem("token")
+	const headers = { 'Authorization': `Bearer ${token}` }
 
-    const token = localStorage.getItem("token")
-    const headers = { 'Authorization': `Bearer ${token}` }
-
-    try {
-        const response = await fetch(`${import.meta.env.VITE_API_URL}/articles/${params.id}`, { headers }) 
-        const data = await response.json()
-        return data
-    } catch(err) {
-        return err
-    }
+	try {
+		const response = await fetch(`${import.meta.env.VITE_API_URL}/articles/${params.id}`, { headers }) 
+		const data = await response.json()
+		return data
+	} catch(err) {
+		return err
+	}
 }
